67b8b1fa7feb9b18340be0742c600471d442fd17
Manuel Guerrini <man.guerrini@gmail.com>
2019-11-05 15:42:55 +0100

    Aggiungo dei tempi di risposta

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // al click sull'hamburger compare il menu
 $(".header-right > a").click(function(){
-  $(".hamburger-menu").show();
+  $(".hamburger-menu").show(500);
 })
 
 // faccio scomparire il menu cliccando sulla X
 $(".hamburger-menu > a").click(function(){
-  $(".hamburger-menu").hide();
+  $(".hamburger-menu").hide(300);
 })
