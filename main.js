abf5bd4cc86602bd1ad48d2e72f26e7c38549c49
Manuel Guerrini <man.guerrini@gmail.com>
2019-11-05 15:41:33 +0100

    clicco sulla X e il menu hamburger scompare

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-// al click sull0hamburger compare il menu
+// al click sull'hamburger compare il menu
 $(".header-right > a").click(function(){
   $(".hamburger-menu").show();
+})
+
+// faccio scomparire il menu cliccando sulla X
+$(".hamburger-menu > a").click(function(){
+  $(".hamburger-menu").hide();
 })
